312d1de549f886697294adcb3fbddb86e3d2c547
EDUARDO CORREIA <97844175+Eduardo1985S@users.noreply.github.com>
2024-10-03 22:20:11 -0300

    Feat: Created files for aula-01

---
+++ aula-01/index.js
@@ -0,0 +1,9 @@
+function myName(name) {
+  console.log(name);
+}
+
+myName('eduardo Correia');
+
+// No terminal, execute o comando node index.js e veja o resultado.
+// Para funcionar, o arquivo index.js deve estar no mesmo diretório que o terminal está apontando, 
+// ou seja, no mesmo diretório que o terminal está aberto (aula-01).
