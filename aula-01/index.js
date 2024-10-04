cb6de733da5e762609ed550a9351c23f2683a97d
EDUARDO CORREIA <97844175+Eduardo1985S@users.noreply.github.com>
2024-10-03 22:23:03 -0300

    Feat: Created the package.json file

--- aula-01/index.js
+++ aula-01/index.js
@@ -7,3 +7,5 @@ myName('eduardo Correia');
 // No terminal, execute o comando node index.js e veja o resultado.
 // Para funcionar, o arquivo index.js deve estar no mesmo diretório que o terminal está apontando, 
 // ou seja, no mesmo diretório que o terminal está aberto (aula-01).
+
+// npm init -y -> cria o arquivo package.json
