718a0e2c8aa561232800fb1b4df847b11b7165b0
EDUARDO CORREIA <97844175+Eduardo1985S@users.noreply.github.com>
2024-10-03 22:36:46 -0300

    Feat: Installed moment library

--- aula-01/index.js
+++ aula-01/index.js
@@ -1,11 +1,16 @@
+const moment = require('moment');
+
 function myName(name) {
-  console.log(name);
+    console.log(name);
+    console.log(moment().format('DD/MM/YYYY HH:mm:ss'));
 }
 
 myName('eduardo Correia');
 
 // No terminal, execute o comando node index.js e veja o resultado.
-// Para funcionar, o arquivo index.js deve estar no mesmo diretório que o terminal está apontando, 
+// Para funcionar, o arquivo index.js deve estar no mesmo diretório que o terminal está apontando,
 // ou seja, no mesmo diretório que o terminal está aberto (aula-01).
 
 // npm init -y -> cria o arquivo package.json
+
+// npm i moment --save -> Essa biblioteca é muito utilizada para manipulação de datas.
